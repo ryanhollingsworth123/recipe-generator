1232bee56aacf6b2be24090a26c40651a2675bb2
ryanhollingsworth123 <resourcefulwriting@gmail.com>
2025-11-21 22:59:08 -0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://ryanhollingsworth123.github.io/recipe-generator"
+  base: "/https://ryanhollingsworth123.github.io/recipe-generator/"
 });
